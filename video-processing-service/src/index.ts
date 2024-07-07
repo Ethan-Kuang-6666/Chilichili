f2865ae59cc0958b3d315256ed6d22193e48dc0b
Yi Kuang <y24kuang@uwaterloo.ca>
2024-07-07 16:05:39 -0400

    Containerize Video Processing Service

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,27 +1,30 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg";
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
-app.post("/process-video", (req, res) => {
+app.post('/process-video', (req, res) => {
     // Get the path of the input video file from the request body
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath;
 
     if (!inputFilePath) {
-        res.status(400).send("Bad Request: Missing inputFilePath.");
+        res.status(400).send('Bad Request: Missing inputFilePath.');
     } else if (!outputFilePath) {
-        res.status(400).send("Bad Request: Missing outputFilePath");
+        res.status(400).send('Bad Request: Missing outputFilePath');
     }
 
-    ffmpeg(inputFilePath).outputOptions("-vf", "scale=-1:360")   // 360p
-    .on("end", () => {
-        res.status(200).send("Processing finished successfully.");
+    ffmpeg(inputFilePath).outputOptions('-vf', 'scale=-1:360')   // 360p
+    .on('end', () => {
+        res.status(200).send('Processing finished successfully.');
     })
-    .on("error", (err) => {
+    .on('error', (err) => {
         console.log(`An error occured: ${err.message}`);
         res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .on('connection', () => {
+        console.log('connected');
     })
     .save(outputFilePath);
 
